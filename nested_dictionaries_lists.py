96eed0ff81000c6e29444badcdc1379c89fc614d
lazaromasot <lazz84@hotmail.com>
2023-04-22 12:57:22 -0400

    Added comments to code.

--- nested_dictionaries_lists.py
+++ nested_dictionaries_lists.py
@@ -8,22 +8,22 @@ sports_directory = {
     "soccer": ["Messi", "Ronaldo", "Rooney"],
 }
 z = [{"x": 10, "y": 20}]
-
-
+# Changing the value in the nested list x
 x[1][0] = 15
 print(x)
-
+# Updating the 'last_name' value in the first dictionary of students list
 students[0]["last_name"] = "Bryant"
 print(students)
-
+# Updating the first element of the "soccer" list in sports_directory dictionary
 sports_directory["soccer"][0] = "Andres"
 print(sports_directory)
-
+# Updating the value of the "y" key in the first dictionary of z list
 z[0]["y"] = 30
 print(z)
+# Function to iterate through a list of dictionaries and print key-value pairs
 
 
-def iterateDictionary(some_list):
+def iterate_dictionary(some_list):
     for dictionary in some_list:
         for key, value in dictionary.items():
             print(f"{key} - {value}", end=", ")
@@ -36,10 +36,12 @@ students = [
     {"first_name": "Mark", "last_name": "Guillen"},
     {"first_name": "KB", "last_name": "Tonel"},
 ]
-iterateDictionary(students)
+iterate_dictionary(students)
+
+# Function to iterate through a list of dictionaries and print the value of a specified key
 
 
-def iterateDictionary2(key_name, some_list):
+def iterate_dictionary2(key_name, some_list):
     for dictionary in some_list:
         print(dictionary[key_name])
 
@@ -50,12 +52,14 @@ students = [
     {"first_name": "Mark", "last_name": "Guillen"},
     {"first_name": "KB", "last_name": "Tonel"},
 ]
-iterateDictionary2("first_name", students)
+iterate_dictionary2("first_name", students)
 
-iterateDictionary2("last_name", students)
+iterate_dictionary2("last_name", students)
+
+# Function to print the number of items and the items themselves for each key in a dictionary
 
 
-def printInfo(some_dict):
+def print_info(some_dict):
     for key, value in some_dict.items():
         print(f"{len(value)} {key.upper()}")
         for item in value:
@@ -76,4 +80,4 @@ dojo = {
         "Devon",
     ],
 }
-printInfo(dojo)
+print_info(dojo)
